31612a91fa0f064a4355dc71d46dd49705c1324a
pavelZaletski <zaleckijpavel@gmail.com>
2017-07-01 10:48:44 +0300

    dfhdrsfh

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://heroku_6q05xjb0:aeamao92plaq974hja2nbhdhs5@ds145292.mlab.com:45292/heroku_6q05xjb0');
+
+mongoose.connection.on('open', function() {
+	console.log('Mongoose connected.');
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
